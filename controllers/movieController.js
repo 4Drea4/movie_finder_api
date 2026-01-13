a5ca3ed27b352fbf7286880a95590ec395bbd274
4drea4 <bakerand@outlook.com>
2026-01-13 14:21:41 -0600

    adding errors

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const { search } = require("../routes/movieRoutes");
 const OMDB_BASE_URL = "http://www.omdbapi.com/"
 
 const searchMovies = async (req,res) => {
@@ -11,13 +12,24 @@ const searchMovies = async (req,res) => {
     return res.status(400).json({error: "Title query parameter is required"});
    }
    const response = await axios.get(OMDB_BASE_URL, {
+    params:{
+        s:title,
+        apikey: process.env.OMDB_API_KEY,
+    },
+   } );
 
-    s 
-    apikey
-   } )
+//    If the request is successful, send the list of movies back to the client as JSON.
+//     Implement try...catch for error handling.
 
 
-}
+}}
+
+//send back list of movies
+res.json(response.data.Search);
+} catch (error) {
+    console.error("Error movie", error.message);
+    res.status(500).json({error:"Failed to fetch"});
+}};
 
 const getMovie = async (req,res) => {
     //
