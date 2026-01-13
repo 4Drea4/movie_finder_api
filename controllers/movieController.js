6dd7fd04049077382fb26ad7b7b4d18529890db5
4drea4 <bakerand@outlook.com>
2026-01-13 14:15:39 -0600

    working it out

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,9 +3,20 @@ const OMDB_BASE_URL = "http://www.omdbapi.com/"
 
 const searchMovies = async (req,res) => {
     try{
-        const 
-    }
-   
+    //    it should get the search term from a query parameter
+        const {title} = req.query;
+    
+   //validation
+   if (!title){
+    return res.status(400).json({error: "Title query parameter is required"});
+   }
+   const response = await axios.get(OMDB_BASE_URL, {
+
+    s 
+    apikey
+   } )
+
+
 }
 
 const getMovie = async (req,res) => {
