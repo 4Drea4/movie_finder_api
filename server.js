3a2ff32cecf75d78e368607ca3f5613beb3f94c1
4drea4 <bakerand@outlook.com>
2026-01-13 13:41:55 -0600

    a bit confused because in the examples we used router.get

--- server.js
+++ server.js
@@ -3,3 +3,17 @@ const express = require('express');
 const app = express();
 const axios = requrie('axios');
 const PORT = process.env.PORT || 3001;
+app.use(express.json());
+
+app.get('./routes/movieRoutes.js',(req,res) => {
+
+})
+app.use('/api/posts', )
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on local host: ${PORT}`)
+})
